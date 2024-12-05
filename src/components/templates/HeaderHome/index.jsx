d0ef68c4b2de8e941005a767f545242de62e5cf8
glassesboyy <aliperwira26@gmail.com>
2024-12-05 17:03:11 +0700

    rapihin code dikit

--- src/components/templates/HeaderHome/index.jsx
+++ src/components/templates/HeaderHome/index.jsx
@@ -4,11 +4,6 @@ import { Button } from "../../atoms";
 const HeaderHome = () => {
   return (
     <header className="flex flex-col items-center justify-center h-1/2 bg-cover bg-center bg-black text-white">
-      <img
-        className="w-full mb-[-400px] h-1/2 object-cover"
-        src="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt=""
-      />
       <h1 className="text-6xl md:text-8xl font-extrabold mb-4 cursor-pointer">
         Movie Zul
       </h1>
